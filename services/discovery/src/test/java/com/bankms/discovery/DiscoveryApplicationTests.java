e4cfe02dd3506dc679dd0251914a9ba3b3f07593
nguyenhoanghiep478 <nguyenhoanghiep478@gmail.com>
2024-07-24 08:26:42 +0700

    set up project

---
+++ services/discovery/src/test/java/com/bankms/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankms.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
